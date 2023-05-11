e46e58a64762b4fa4b1b230ab7bf08942ce90003
Danil Pnevsky <dpnevsky@gmail.com>
2023-05-11 15:50:15 +0300

    delete AuthProviderImpl.java and modify SecurityConfig.java

--- src/main/java/com/pnevsky/spring_security/config/SecurityConfig.java
+++ src/main/java/com/pnevsky/spring_security/config/SecurityConfig.java
@@ -1,24 +1,30 @@
 package com.pnevsky.spring_security.config;
 
-import com.pnevsky.spring_security.security.AuthProviderImpl;
-
+import com.pnevsky.spring_security.security.PersonDetails;
+import com.pnevsky.spring_security.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private AuthProviderImpl authProvider;
+    private PersonDetailsService personDetailsService;
 
-    @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
+    }
 
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder){
-        authenticationManagerBuilder.authenticationProvider(authProvider);
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
