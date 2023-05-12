11fc870279f473c9831d35ef993a23614e27ec7c
Danil Pnevsky <dpnevsky@gmail.com>
2023-05-12 21:48:22 +0300

    add encryption password

--- src/main/java/com/pnevsky/spring_security/services/RegistrationService.java
+++ src/main/java/com/pnevsky/spring_security/services/RegistrationService.java
@@ -3,6 +3,7 @@ package com.pnevsky.spring_security.services;
 import com.pnevsky.spring_security.models.Person;
 import com.pnevsky.spring_security.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,15 +11,18 @@ import javax.transaction.Transactional;
 @Service
 public class RegistrationService {
 
-    PersonRepository personRepository;
+    private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PersonRepository personRepository) {
+    public RegistrationService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
         this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person){
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         personRepository.save(person);
     }
 
